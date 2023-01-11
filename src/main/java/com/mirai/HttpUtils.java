36046bb3d8cd0417b6191b094dd130e0784babfd
Lin <2862125721@qq.com>
2023-01-11 22:42:17 +0800

    nope

--- src/main/java/com/mirai/HttpUtils.java
+++ src/main/java/com/mirai/HttpUtils.java
@@ -1,11 +1,18 @@
 package com.mirai;
 
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
 import net.mamoe.mirai.utils.ExternalResource;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
 
 public final class HttpUtils {
     // 用于返回图片资源
@@ -17,4 +24,14 @@ public final class HttpUtils {
         return ExternalResource.create(bytes);
     }
 
+    public static String QrDecode(String filepath) throws IOException, NotFoundException {
+        BufferedImage bufferedImage = ImageIO.read(new FileInputStream(filepath));
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        Binarizer binarizer = new HybridBinarizer(source);
+        BinaryBitmap bitmap = new BinaryBitmap(binarizer);
+        HashMap<DecodeHintType, Object> decodeHints = new HashMap<>();
+        decodeHints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+        Result result = new MultiFormatReader().decode(bitmap, decodeHints);
+        return result.getText();
+    }
 }
