59369968e569f7a5227dccba9fcc69b9726cb060
Lin <2862125721@qq.com>
2024-07-18 19:10:31 +0800

    重构了RankMusic和RecentMusic,优化了战力算法

--- src/main/java/com/dancecube/ratio/RankMusicInfo.java
+++ src/main/java/com/dancecube/ratio/RankMusicInfo.java
@@ -1,60 +1,50 @@
 package com.dancecube.ratio;
 
+
 import com.google.gson.JsonObject;
 
-import java.util.ArrayList;
-
 /**
- * 排名记录音乐信息 (Best)
+ * RankMusic的一个其中难度/谱面记录
  */
 public class RankMusicInfo extends RecordedMusicInfo {
-    private boolean official;
-    private final ArrayList<SingleRank> accList = new ArrayList<>();
+    private final boolean isOfficial;
+    private final int ranking;
 
-    public static RankMusicInfo get(JsonObject object) {
-        RankMusicInfo musicInfo = new RankMusicInfo();
-        musicInfo.id = object.get("MusicID").getAsInt();
-        musicInfo.name = object.get("Name").getAsString();
-        musicInfo.official = object.get("OwnerType").getAsInt()==1;
-        object.get("ItemRankList").getAsJsonArray().forEach(element -> {
-            JsonObject json = element.getAsJsonObject();
-            float acc = json.get("PlayerPercent").getAsFloat() / 100;
-            int level = json.get("MusicRank").getAsInt();
-            int difficulty = json.get("MusicLevOld").getAsInt();
-            int rank = json.get("MusicRanking").getAsInt();
-            int score = json.get("PlayerScore").getAsInt();
-            int combo = json.get("ComboCount").getAsInt();
-            int miss = json.get("PlayerMiss").getAsInt();
-            musicInfo.accList.add(new SingleRank(difficulty, level, acc, score, rank, combo, miss));
-        });
-        return musicInfo;
+    public int getRanking() {
+        return ranking;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public int getId() {
-        return this.id;
-    }
-
-    public boolean isOfficial() {
-        return official;
-    }
-
-    @Override
-    public float getBestRatio() {
-        return getBestInfo().ratio;
-    }
-
-    public SingleRank getBestInfo() {
-        accList.sort((o1, o2) -> Float.compare(o2.ratio, o1.ratio));
-//        return Math.round(accList.get(0).ratio);
-        return accList.get(0);
+    /**
+     * 通过ItemRankList的列表分别创建对象，需要在创建时解析好源JSON并传递id name ownerType!!!
+     */
+    public RankMusicInfo(int id, String name, int ownerType, JsonObject details) {
+        super(
+                id, name, details.get("MusicLevOld").getAsInt(),
+                details.get("MusicRank").getAsInt(),
+                details.get("MusicLev").getAsInt(),
+                details.get("PlayerPercent").getAsFloat() / 100,
+                details.get("PlayerScore").getAsInt(),
+                details.get("ComboCount").getAsInt(),
+                details.get("PlayerMiss").getAsInt()
+        );
+        ranking = details.get("MusicRanking").getAsInt();
+        isOfficial = ownerType==1;
     }
 
     @Override
     public String toString() {
-        return "Name: %s\n#Ratio: %s\n#BestRatio: %.2f\n".formatted(name, accList, getBestRatio());
+        return "RankMusicInfo{" +
+               "difficulty=" + getDifficulty() +
+               ", level=" + getLevel() +
+               ", acc=" + getAccuracy() +
+               ", ratio=" + getRatio() +
+               '}';
+    }
+
+    @Override
+    public boolean isOfficial() {
+        return isOfficial;
     }
 }
+
